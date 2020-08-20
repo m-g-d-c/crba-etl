9731c9e76376b1f7a3f180613086404df7ffc7e7
m-g-d-c <46639713+m-g-d-c@users.noreply.github.com>
2020-08-20 17:59:11 +0200

    Create save_cleansed_data.py

---
+++ cleanse/save_cleansed_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+# Define the export path for all RAW data sources
+cwd = os.getcwd()
+
+data_cleansed = cwd + "\data\data_cleansed\\"
+
+def save_cleansed_data(dataframe, filename, output_path = data_cleansed):
+    """Save raw data 
+
+    Parameters:
+    dataframe (obj): Pandas dataframe to be stored
+    filename (string): The way you would like o anem your file. Must include the extension (for example .xlsx)
+    output_path (string): Folder where data is stored
+
+    Returns:
+    obj: 
+
+   """
+    dataframe.to_excel(output_path + filename)
+    print('The raw data has been saved as .xlsx file in: ' + output_path)
