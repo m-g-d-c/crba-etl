8d05dea6bed9b56c6c21b3db6d91bbd0b7ffdaad
m-g-d-c <46639713+m-g-d-c@users.noreply.github.com>
2020-08-20 17:59:04 +0200

    Update unesco_cleanser.py
    Add functonality of right join

--- cleanse/unesco_cleanser.py
+++ cleanse/unesco_cleanser.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-def cleanse_unesco_api_data(uis_data, country_iso2_list, columns, most_recent_only = True):
+def cleanse_unesco_api_data(uis_data, country_df, country_df_iso2_col, columns, most_recent_only = True):
     """< To do >
 
     Parameters:
-    s15_raw (obj): Should be return of function s55_extract or s56_extract
-    country_iso2_list (array): A numpy array of 2-character country codes.
+    uis_data (obj): The return of the extract function of extract_unesco_api_data
+    country_df (obj): Pandas dataframe containing the final country list to be used in CRBA including country names, and their ISO2 and ISO 3 code.
+    counry_df_iso2_col (str): The column containing the ISO codes in the country_df dataframe. Specified as string.
     columns: List of the dimensions (i.e. columns) that are you want to keep for the indicator beyond the main necessary ones
+    most_recent_only (bool): Execute the group by statement to only kee the most recent observation per country. 
 
     Returns:
     obj: Returns pandas dataframe with the relevant data of the indicator
@@ -16,13 +18,18 @@ def cleanse_unesco_api_data(uis_data, country_iso2_list, columns, most_recent_on
     columns_kept = ['REF_AREA', 'TIME_PERIOD','OBS_VALUE'] + columns
     uis_data = uis_data[columns_kept]
     
-    # Discard rows of countries that are in the master country list
-    uis_data = uis_data[uis_data.REF_AREA.isin(country_iso2_list)]
-    
     if most_recent_only == True:
         # Retrieve the most up-to-date number for each country
         uis_data = uis_data[uis_data['TIME_PERIOD']==uis_data.groupby('REF_AREA')['TIME_PERIOD'].transform('max')]
     else:
         pass
+
+    # Discard rows of countries that are not in the master country list
+    cleansed_final = uis_data.merge(
+        right = country_df,
+        how = 'right', 
+        left_on = 'REF_AREA',
+        right_on = country_df_iso2_col,
+        validate = 'one_to_one').assign(indicator = 'I-15')
     
-    return(uis_data)
+    return(cleansed_final.sort_values(by = country_df_iso2_col, axis = 0))
