28ea90f7f95c8aa38a7201f63c7ee1ee5f9c21e1
m-g-d-c <46639713+m-g-d-c@users.noreply.github.com>
2020-08-22 11:40:54 +0200

    Update unesco_cleanser.py
    Modify function to keep all columns (incl dimensions). The code is taken over from sdg_extractor.

--- cleanse/unesco_cleanser.py
+++ cleanse/unesco_cleanser.py
@@ -1,35 +1,42 @@
 import pandas as pd
 
-def cleanse_unesco_api_data(uis_data, country_df, country_df_iso2_col, columns, most_recent_only = True):
-    """< To do >
+def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df_iso2_col):
+    """Cleanse raw data from UNESCO API
+
+    Retrieve only the latest observations for each country-dimension group. NB Dimensions make are required to uniquely identify a row of the dataset and therefore their values define groups, e.g. male vs femeale in a country.
+    Discard countries that aren't in the CRBA final list of countries
 
     Parameters:
-    uis_data (obj): The return of the extract function of extract_unesco_api_data
-    country_df (obj): Pandas dataframe containing the final country list to be used in CRBA including country names, and their ISO2 and ISO 3 code.
-    counry_df_iso2_col (str): The column containing the ISO codes in the country_df dataframe. Specified as string.
-    columns: List of the dimensions (i.e. columns) that are you want to keep for the indicator beyond the main necessary ones
-    most_recent_only (bool): Execute the group by statement to only kee the most recent observation per country. 
+    raw_data (obj): Return of function 'extract_sdg_api_data'. Should be a pandas dataframe.
+    raw_data_iso_2_col (str): Name of the column in 'raw data' frame that contains the ISO2 country codes.
+    country_df (obj): Dataframe containing the final CRBA list of countries
+    country_df_iso2_col (str): Column in 'country_df', which contains the country iso2 codes.
 
     Returns:
-    obj: Returns pandas dataframe with the relevant data of the indicator
+    obj: Returns pandas dataframe, which only contains the countries meant to be in CRBA and the latest observed value for the indiator.
 
    """
-    # Discard unnecessay columns
-    columns_kept = ['REF_AREA', 'TIME_PERIOD','OBS_VALUE'] + columns
-    uis_data = uis_data[columns_kept]
-    
-    if most_recent_only == True:
-        # Retrieve the most up-to-date number for each country
-        uis_data = uis_data[uis_data['TIME_PERIOD']==uis_data.groupby('REF_AREA')['TIME_PERIOD'].transform('max')]
-    else:
-        pass
 
-    # Discard rows of countries that are not in the master country list
-    cleansed_final = uis_data.merge(
+    # Group by to obtain the latest available value per group, where group is 'col_list_gb'
+        # Create list of all column to group by
+    col_list = raw_data.columns.to_list() # list of all columns in the dataframe
+    col_list_gb = [e for e in col_list if e not in ('OBS_VALUE', 'TIME_PERIOD')] # exclude timePeriodStart and value, because these one'saren't used for the groupby statement
+
+        # Some of the columns contain values which Python interprets as lists (e.g. [8.1]). These make the groupby statement malfunction. Convert them to string
+    raw_data[col_list_gb] =  raw_data[col_list_gb].astype(str)
+
+        # Retreive the latest available data for each group, where group is 'col_list_gb'
+    grouped_data = raw_data[raw_data['TIME_PERIOD'] == raw_data.groupby(col_list_gb)['TIME_PERIOD'].transform('max')]
+
+
+    # Discard countries that aren't part of the final CRBA master list
+    grouped_data_iso_filt = grouped_data.merge(
         right = country_df,
-        how = 'right', 
-        left_on = 'REF_AREA',
+        how = 'right',
+        left_on = raw_data_iso_2_col,
         right_on = country_df_iso2_col,
-        validate = 'one_to_one').assign(indicator = 'I-15')
-    
-    return(cleansed_final.sort_values(by = country_df_iso2_col, axis = 0))
+        indicator = True,
+        validate = 'many_to_one')
+
+    # return result
+    return(grouped_data_iso_filt.sort_values(by = country_df_iso2_col, axis = 0))
