d4bd7b128155d46a33fb7e52ef9966ea7fbb69ef
m-g-d-c <46639713+m-g-d-c@users.noreply.github.com>
2020-08-24 17:58:32 +0200

    Update unesco_cleanser.py
    Add functionality of dealing with dimensions and being able to pass columns which are dimensions and which should not be included in the group by statement as list in the parameters.

--- cleanse/unesco_cleanser.py
+++ cleanse/unesco_cleanser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df_iso2_col):
+def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df_iso2_col, non_dim_cols):
     """Cleanse raw data from UNESCO API
 
     Retrieve only the latest observations for each country-dimension group. NB Dimensions make are required to uniquely identify a row of the dataset and therefore their values define groups, e.g. male vs femeale in a country.
@@ -11,6 +11,7 @@ def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df
     raw_data_iso_2_col (str): Name of the column in 'raw data' frame that contains the ISO2 country codes.
     country_df (obj): Dataframe containing the final CRBA list of countries
     country_df_iso2_col (str): Column in 'country_df', which contains the country iso2 codes.
+    non_dim_cols (list): List of columns, which should be excluded from the group by statement (that is, if a column is not indicated here, then the maximum value for each of its values will be calculated)
 
     Returns:
     obj: Returns pandas dataframe, which only contains the countries meant to be in CRBA and the latest observed value for the indiator.
@@ -20,7 +21,8 @@ def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df
     # Group by to obtain the latest available value per group, where group is 'col_list_gb'
         # Create list of all column to group by
     col_list = raw_data.columns.to_list() # list of all columns in the dataframe
-    col_list_gb = [e for e in col_list if e not in ('OBS_VALUE', 'TIME_PERIOD')] # exclude timePeriodStart and value, because these one'saren't used for the groupby statement
+    non_dim_cols_tuple = tuple(non_dim_cols) # parameters must be passed as list, but the following command requires a tuple
+    col_list_gb = [e for e in col_list if e not in non_dim_cols_tuple] # exclude timePeriodStart and value, because these one'saren't used for the groupby statement
 
         # Some of the columns contain values which Python interprets as lists (e.g. [8.1]). These make the groupby statement malfunction. Convert them to string
     raw_data[col_list_gb] =  raw_data[col_list_gb].astype(str)
