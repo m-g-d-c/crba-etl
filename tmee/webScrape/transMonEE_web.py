bbde896e491c4e8abee94504f7ae5a9f88ddf9d4
James Cranwell-Ward <jcranwellward@unicef.org>
2020-08-28 08:50:50 +0000

    WIP of IDE config and environment bootstrap

--- tmee/webScrape/transMonEE_web.py
+++ tmee/webScrape/transMonEE_web.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 from fileUtils import fileDownload
 
@@ -6,8 +5,10 @@ from fileUtils import fileDownload
 def get_countries(url_transmonee):
     page = fileDownload.api_request(url_transmonee)
     # parse HTML
-    page_html = BeautifulSoup(page.content, 'html.parser')
+    page_html = BeautifulSoup(page.content, "html.parser")
     country_soup = page_html.findAll("div", {"class": "left-nav"})
     # get text from html lines into list of countries
-    country_names = [line.get_text(strip=True).lower() for line in country_soup[0].findAll('a')]
+    country_names = [
+        line.get_text(strip=True).lower() for line in country_soup[0].findAll("a")
+    ]
     return country_names
