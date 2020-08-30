adfe3b7e5075584eeaa738e7b2b4f818715d157c
m-g-d-c <46639713+m-g-d-c@users.noreply.github.com>
2020-08-30 18:52:35 +0200

    Update save_raw_data.py
    Improve documentation and clean up file.

--- extract/save_raw_data.py
+++ extract/save_raw_data.py
@@ -7,7 +7,7 @@ cwd = os.getcwd()
 data_sources_raw = cwd + "\data\data_raw\\"
 
 def save_raw_data(dataframe, filename, output_path = data_sources_raw):
-    """Save raw data 
+    """Save raw data
 
     Parameters:
     dataframe (obj): Pandas dataframe to be stored
@@ -15,8 +15,12 @@ def save_raw_data(dataframe, filename, output_path = data_sources_raw):
     output_path (string): Folder where data is stored
 
     Returns:
-    obj: 
+    obj:
 
    """
+
+    # Save data
     dataframe.to_excel(output_path + filename)
+
+    # Provide log for user
     print('The raw data has been saved as .xlsx file in: ' + output_path)
