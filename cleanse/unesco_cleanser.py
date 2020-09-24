f888546bd806355944260d55fe39b56bb52639e8
James Cranwell-Ward <jcranwellward@unicef.org>
2020-09-24 11:20:43 +0000

    WIP refactor

--- cleanse/unesco_cleanser.py
+++ cleanse/unesco_cleanser.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df_iso2_col, non_dim_cols):
+
+def cleanse_unesco_api_data(
+    raw_data, raw_data_iso_2_col, country_df, country_df_iso2_col, non_dim_cols
+):
     """Cleanse raw data from UNESCO API
 
     Retrieve only the latest observations for each country-dimension group. NB Dimensions make are required to uniquely identify a row of the dataset and therefore their values define groups, e.g. male vs femeale in a country.
@@ -19,25 +22,33 @@ def cleanse_unesco_api_data(raw_data, raw_data_iso_2_col, country_df, country_df
    """
 
     # Group by to obtain the latest available value per group, where group is 'col_list_gb'
-        # Create list of all column to group by
-    col_list = raw_data.columns.to_list() # list of all columns in the dataframe
-    non_dim_cols_tuple = tuple(non_dim_cols) # parameters must be passed as list, but the following command requires a tuple
-    col_list_gb = [e for e in col_list if e not in non_dim_cols_tuple] # exclude timePeriodStart and value, because these one'saren't used for the groupby statement
+    # Create list of all column to group by
+    col_list = raw_data.columns.to_list()  # list of all columns in the dataframe
+    non_dim_cols_tuple = tuple(
+        non_dim_cols
+    )  # parameters must be passed as list, but the following command requires a tuple
+    col_list_gb = [
+        e for e in col_list if e not in non_dim_cols_tuple
+    ]  # exclude timePeriodStart and value, because these one'saren't used for the groupby statement
 
-        # Some of the columns contain values which Python interprets as lists (e.g. [8.1]). These make the groupby statement malfunction. Convert them to string
-    raw_data[col_list_gb] =  raw_data[col_list_gb].astype(str)
+    # Some of the columns contain values which Python interprets as lists (e.g. [8.1]). These make the groupby statement malfunction. Convert them to string
+    raw_data[col_list_gb] = raw_data[col_list_gb].astype(str)
 
-        # Retreive the latest available data for each group, where group is 'col_list_gb'
-    grouped_data = raw_data[raw_data['TIME_PERIOD'] == raw_data.groupby(col_list_gb)['TIME_PERIOD'].transform('max')]
+    # Retreive the latest available data for each group, where group is 'col_list_gb'
+    grouped_data = raw_data[
+        raw_data["TIME_PERIOD"]
+        == raw_data.groupby(col_list_gb)["TIME_PERIOD"].transform("max")
+    ]
 
     # Discard countries that aren't part of the final CRBA master list
     grouped_data_iso_filt = grouped_data.merge(
-        right = country_df,
-        how = 'right',
-        left_on = raw_data_iso_2_col,
-        right_on = country_df_iso2_col,
-        indicator = True,
-        validate = 'many_to_one')
+        right=country_df,
+        how="right",
+        left_on=raw_data_iso_2_col,
+        right_on=country_df_iso2_col,
+        indicator=True,
+        validate="many_to_one",
+    )
 
     # return result
-    return(grouped_data_iso_filt.sort_values(by = country_df_iso2_col, axis = 0))
+    return grouped_data_iso_filt.sort_values(by=country_df_iso2_col, axis=0)
--- extract/__init__.py
+++ extract/__init__.py
@@ -1,1 +1,72 @@
+from abc import ABC, abstractmethod, abstractproperty
+
 import pandas as pd
+import requests
+
+
+class Extractor(ABC):
+    """Abstract base class for extractor classes
+    """
+
+    type = None
+
+    @classmethod
+    def content_type(cls):
+        return cls.type
+
+    @classmethod
+    @abstractmethod
+    def data(cls, url):
+        """Performs the actual extraction of the data from the provided URL 
+
+        Args:
+            url (string): URL to fetch the data from
+
+        Returns:
+            DataFrame: DataFrame contianing the data
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def extract(cls, url):
+
+        raw_data = cls.data(url)
+
+        # Log functionality for user
+        print(
+            "The following columns are present in the datasets, and this is the number of unique values they have. "
+        )
+        for col in raw_data:
+            print(
+                "The column {} has {} unique values.".format(
+                    col, raw_data[col].astype(str).nunique()
+                )
+            )
+
+        # Return pandas dataframe
+        return raw_data
+
+
+class CSVExtractor(Extractor):
+
+    type = "csv"
+
+    @classmethod
+    def data(cls, url):
+
+        raw_data = pd.read_csv(url, sep=",",)
+
+        return raw_data
+
+
+class JSONExtractor(Extractor):
+
+    type = "json"
+
+    @classmethod
+    def data(cls, url):
+        # Extract data and convert to pandas dataframe
+        raw_data = pd.json_normalize(requests.get(url).json()["data"])
+
+        return raw_data
+
